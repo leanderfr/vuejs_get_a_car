0015486397d50f5d831db7811ed84cf793d74cbb
leanderfr <leanderprogrammer@gmail.com>
2025-11-24 16:05:11 -0300

    Atl

--- backend_node_js/controllers/expressionController.js
+++ backend_node_js/controllers/expressionController.js
@@ -6,27 +6,39 @@ const { Op } = require('sequelize');
 //************************************************************************************
 
 exports.getByCountry = async (req, res) => {
-    let fieldsToSelect = ['id', 'item', 'active'];
 
-    if (req.params.country === 'usa') {
-      fieldsToSelect.push( ['english', 'language'] );
+    let fieldsToSelect = []
+
+    if (req.params.resultformat=='reference') {
+      fieldsToSelect.push( 'item' )
+
+      if (req.params.country === 'usa') {
+        fieldsToSelect.push( ['english', 'language'] );
+      }
+      if (req.params.country === 'brazil') {
+        fieldsToSelect.push( ['portuguese', 'language'] );
+      }
+
     }
-    if (req.params.country === 'brazil') {
-      fieldsToSelect.push( ['portuguese', 'language'] );
+
+    if (req.params.resultformat=='json') {
+      fieldsToSelect.push( 'id', 'active', 'english', 'portuguese' );
+
+      if (req.params.searchbox) {
+        conditions.push({ item: req.params.searchbox });
+      }
     }
 
     const conditions = [ {
       active: req.params.active,
     }];
 
-    if (req.params.searchbox) {
-      conditions.push({ item: req.params.searchbox });
-    }
-
     const expressions = await Expression.findAll({
       where: {[Op.and]: conditions},
       attributes: fieldsToSelect 
     })
+
+    res.setHeader('Content-Type', 'application/json');
     res.json(expressions);
 };  
 
