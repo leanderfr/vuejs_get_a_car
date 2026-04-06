0f5d1a6981afdc6b3ec30e473b56ef26b38231d4
leanderfr <leanderprogrammer@gmail.com>
2026-04-06 15:04:43 -0300

    Atl

--- prisma_nodejs_backend/src/create.ts
+++ prisma_nodejs_backend/src/create.ts
@@ -1,2 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import { networkInterfaces } from 'node:os'
 
+const prisma = new PrismaClient()
+
+async function main() {
+
+  try {
+    const res = await prisma.bookings.createMany({
+      data: [ {dropoff_datetime: networkInterfaces()} ]
+    })
+  }
+  catch (err) {
+
+  }
+
+}
